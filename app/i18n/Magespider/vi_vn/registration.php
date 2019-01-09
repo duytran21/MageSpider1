1f9e5f9fd7c82c19afce9a36db99657d76503bc6
duytran21 <alanduy21@yahoo.com>
2019-01-09 01:45:50 -0800

    Add Vietnamese package language

---
+++ app/i18n/Magespider/vi_vn/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magespider_vi_vn',
+    __DIR__
+);
